05ca5b5486e5db1718da9ba08180c72ba0509e1d
linqueta <lincolnrodrs@gmail.com>
2019-09-21 20:18:16 -0300

    update gems

--- alice/config/application.rb
+++ alice/config/application.rb
@@ -8,12 +8,13 @@ require 'active_job/railtie'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
+require 'action_mailer/railtie'
 require 'sprockets/railtie'
 Bundler.require(*Rails.groups)
 
 module Alice
   class Application < Rails::Application
-    config.load_defaults 5.2
+    config.load_defaults 6.0
     config.generators.system_tests = nil
   end
 end
